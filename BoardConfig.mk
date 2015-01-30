5de2453f119756d3def29b0e1696fbdcf89b9fd4
Luis de Lucas <luisetex84@outlook.com>
2015-01-30 22:50:37 +0100

    Some TWRP adjustments

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,11 @@
 USE_CAMERA_STUB := true
 
 # inherit from the proprietary version
--include vendor/bq/kaito_wifi/BoardConfigVendor.mk
+#-include vendor/bq/kaito_wifi/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
-#TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := MT8151
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := mt8392
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -13,11 +13,7 @@ TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := Aquaris_E10
-
-BOARD_KERNEL_CMDLINE := 
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE := 2048
+#TARGET_BOOTLOADER_BOARD_NAME := Aquaris_E10
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x1400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x1400000
@@ -29,6 +25,11 @@ BOARD_FLASH_BLOCK_SIZE := 0x20000
 #TARGET_KERNEL_CONFIG := cyanogenmod_kaito_wifi_defconfig
 # Keep this as a fallback
 TARGET_PREBUILT_KERNEL := device/bq/kaito_wifi/kernel
+BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_CUSTOM_BOOTIMG_MK := device/bq/kaito_wifi/boot.mk
+BOARD_MKBOOTIMG_ARGS := --base 0x10000000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100 --board 7f0af25
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
@@ -37,6 +38,6 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 DEVICE_RESOLUTION := 1920x1200
 RECOVERY_SDCARD_ON_DATA := true
-BRIGHTNESS_SYS_FILE := "/sys/devices/platform/leds-mt65xx/leds/lcd-backlight"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
-BOARD_CUSTOM_BOOTIMG_MK := device/bq/kaito_wifi/boot.mk
+TW_NO_CPU_TEMP := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun0/file"
